8a83875490eb34dcaabc28166bd78945b5394fbb
alessandro.pasqua <alessandro.pasqua@buonarroti.tn.it>
2023-09-21 14:34:15 +0200

    feat: - Added constructor method Tubo

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,4 +1,16 @@
 public class Tubo {
     private float lunghezza;
     private float diametro;
+
+    public Tubo(float diametro, float lunghezza) throws Exception {
+        if(diametro < 0) {
+            throw new Exception("\nDiametro non valido!");
+        } else {
+            if(lunghezza < 0) {
+                lunghezza = lunghezza * -1;
+            }
+            this.lunghezza = lunghezza;
+            this.diametro = diametro;
+        }
+    }
 }
